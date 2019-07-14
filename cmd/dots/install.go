97039fc0a42e32b3c120d27c89e15a07cd55e2ad
Evan Purkhiser <evanpurkhiser@gmail.com>
2019-07-14 04:00:58 -0700

    Semi-working install

--- cmd/dots/install.go
+++ cmd/dots/install.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
 
 	"go.evanpurkhiser.com/dots/installer"
@@ -13,10 +11,24 @@ var installCmd = cobra.Command{
 	Use:   "install [filter...]",
 	Short: "Install and compile dotfiles from sources",
 	RunE: func(cmd *cobra.Command, args []string) error {
+		forceReInstall, _ := cmd.Flags().GetBool("reinstall")
+		dryRun, _ := cmd.Flags().GetBool("dry-run")
+
 		dotfiles := resolver.ResolveDotfiles(*sourceConfig, *sourceLockfile).Filter(args)
 		prepared := installer.PrepareDotfiles(dotfiles, *sourceConfig)
 
-		fmt.Println(prepared)
+		if dryRun {
+			// TODO: Do logging output
+			return nil
+		}
+
+		config := installer.InstallConfig{
+			SourceConfig:   sourceConfig,
+			ForceReinstall: forceReInstall,
+		}
+
+		installer.InstallDotfiles(prepared, config)
+		installer.RunInstallScripts(prepared, config)
 
 		return nil
 	},
@@ -27,7 +39,7 @@ func init() {
 	flags := installCmd.Flags()
 	flags.SortFlags = false
 
-	flags.BoolP("reinstall", "r", false, "reinstall all dotfiles, includes unchanged files")
+	flags.BoolP("reinstall", "r", false, "forces execution of all installation scripts")
 	flags.BoolP("dry-run", "n", false, "do not mutate any dotfiles, implies info")
 	flags.BoolP("info", "i", false, "prints install operation details")
 	flags.BoolP("verbose", "v", false, "prints debug data, implies info")
